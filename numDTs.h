6662341c0ea5ede1d6c7a0748962b4e92268c3c5
keepcry1ng <skimprogramming@gmail.com>
2026-01-27 21:31:39 -0800

    Add files via upload

---
+++ numDTs.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <stdint.h>
+#include <string>
+#include <stdexcept>
+// Mostly low level stuff, just working with bit masks, bit operations, etc.
+
+struct numDT1 {
+	/*
+	Properties:
+	Byte 1 used as multiplier
+	Byte 2 used as multiplicand
+	Bit 7 of byte 1(multiplier) used as adding +1
+	*/
+private:
+	uint8_t high = 0b00000000;
+	uint8_t low = 0b00000000;
+public:
+	numDT1() = default;
+	numDT1(uint8_t high_, uint8_t low_) : high(high_), low(low_) {};
+	numDT1(uint16_t num) { high = num >> 8; low = num & 0xFF; };
+
+	uint16_t const read() {
+		return ((uint16_t)high) << 8 | low;
+	}
+
+	void write(const uint8_t high_ = 0b00000000, const uint8_t low_ = 0b00000000) {
+		high = high_;
+		low = low_;
+	}
+
+	uint16_t const val() {
+		uint8_t flag = (high & 0b10000000) ? 1 : 0;
+		uint8_t multiplier = high & 0b01111111;
+
+		return multiplier * low + flag;
+	}
+};
+
+uint8_t parseByte(std::string s) {
+	if (s.size() != 8) throw std::invalid_argument("Must be 8 bits long");
+	uint8_t result = 0;
+	for (const auto& c : s) {
+		result <<= 1;
+		if (c == '1') result |= 1;
+		else if (c != '0') throw std::invalid_argument("Invalid character");
+	}
+	return result;
+}
